e2ac53f36f67499b07ed708a7b3679400c84e947
Prittam Ravi <prittam867@gmail.com>
2024-03-31 19:45:35 +0800

    Add barebones of twitter api

---
+++ airflow/dags/twitter_etl.py
@@ -0,0 +1,38 @@
+from twikit import Client
+from airflow.decorators import dag, task
+import logging
+from datetime import datetime
+
+@dag(schedule_interval='@daily', start_date=datetime(2021, 1, 1), catchup=False)
+def twitter_etl():
+
+    @task
+    def print_hello():
+        logging.info('Hello World')
+        return 0
+
+    @task
+    def extract():
+        # Enter your account information
+        USERNAME = '///'
+        EMAIL = '///'
+        PASSWORD = '///'
+
+        client = Client('en-US')
+        client.login(
+            auth_info_1=USERNAME,
+            auth_info_2=EMAIL,
+            password=PASSWORD
+        )
+        tweets = client.search_tweet('Trump', 'Top')
+        logging.info('Tweets extracted, count: %s', len(tweets))
+        for tweet in tweets:
+            logging.info('Tweet: %s', tweet)
+            logging.info('Tweet text: %s', tweet.text)
+        return 0
+
+    task1 = print_hello()
+    task2 = extract()
+    task1 >> task2
+
+twitter_etl_dag = twitter_etl()
