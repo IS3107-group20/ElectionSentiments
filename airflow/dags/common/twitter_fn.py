9b0ed937593b45ba72be65738cee2d14d51cacb2
Prittam Ravi <prittam867@gmail.com>
2024-04-13 01:52:28 +0800

    Add transform and load to json function

--- airflow/dags/common/twitter_fn.py
+++ airflow/dags/common/twitter_fn.py
@@ -1,4 +1,8 @@
 import logging
+import re
+import pandas as pd
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
 from twikit import Client
 from airflow.models import Variable
 
@@ -30,7 +34,7 @@ def _extract_tweets_n(number_of_tweets: int, search: str):
                 'favorite_count': tweet.favorite_count,
                 'reply_count': tweet.reply_count,
                 'country' : tweet.user.location
-            }`
+            }
             all_tweets.append(curr_tweet)
             if len(all_tweets) == number_of_tweets:
                 break
@@ -38,5 +42,45 @@ def _extract_tweets_n(number_of_tweets: int, search: str):
     logger.info('Tweets extracted, count: %s', len(all_tweets))
     return all_tweets
 
+
+def tweet_cleaning(tweet):
+    tweet = re.sub(r"http\S+|www\S+|https\S+", '', tweet, flags=re.MULTILINE)
+    tweet = re.sub(r'@\w+', '', tweet)
+    tweet = re.sub(r'#', '', tweet)
+    tweet = re.sub(r'RT[\s]+', '', tweet)
+    tweet = re.sub(r"[^a-zA-Z\s:;=)(]", '', tweet)
+    tweet = re.sub(r'\s+', ' ', tweet).strip()
+    
+    return tweet
+
+def _transform_tweets(all_tweets):
+    nltk.download('vader_lexicon')
+    sia = SentimentIntensityAnalyzer()
+    # Convert to dataframe
+    tweets_df = pd.DataFrame(all_tweets)
+    
+    def analyze_sentiment(text):
+        sentiment_scores = sia.polarity_scores(text)
+        return sentiment_scores['compound']
+        
+    def classify_sentiment(score):
+        if score >= 0.05:
+            return 'Positive'
+        elif score <= -0.05:
+            return 'Negative'
+        else:
+            return 'Neutral'
+    tweets_df['cleaned_text'] = tweets_df['text'].apply(tweet_cleaning)
+    tweets_df['sentiment_score'] = tweets_df['cleaned_text'].apply(analyze_sentiment)
+    tweets_df['sentiment'] = tweets_df['sentiment_score'].apply(classify_sentiment)
+
+    logger.info(tweets_df.dtypes)
+    return tweets_df
+
+
+def _load_to_json(df):
+    json_result = df.to_json(orient='records', lines=True)
+    logger.info(json_result)
+    return json_result
 
     
--- airflow/dags/twitter_etl.py
+++ airflow/dags/twitter_etl.py
@@ -1,8 +1,6 @@
-from twikit import Client
 from airflow.decorators import dag, task
 import logging
-from common.twitter_fn import _extract_tweets_n
-from airflow.models import Variable
+from common.twitter_fn import _extract_tweets_n, _transform_tweets, _load_to_json
 from datetime import datetime
 
 logger = logging.getLogger(__name__)
@@ -12,24 +10,29 @@ def twitter_etl():
 
     @task
     def print_hello():
-        logging.info('Hello World')
+        logger.info('Hello World')
         return 0
 
     @task
     def extract():
-        search_params = ['Trump', 'Biden', 'US Presidential Election']
+        search_params = ['Trump']
         tweets = []
         for search in search_params:
-            tweets.append(_extract_tweets_n(20, search))
+            tweets += (_extract_tweets_n(20, search))
         return tweets
     
     @task 
     def transform(data: list):
-        return 0
+        return _transform_tweets(data)
+    
+    @task
+    def load(df):
+        return _load_to_json(df)
 
     task1 = print_hello()
     task2 = extract()
-    task3 = transform(task2)  
+    task3 = transform(task2)
+    task4 = load(task3)  
     task1 >> task2 >> task3
 
 twitter_etl_dag = twitter_etl()
